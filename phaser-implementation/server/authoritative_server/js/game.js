6f777734d735a040233012c81590b6b2cf6ac743
aidyp <aidan.j.patrick@gmail.com>
2021-09-14 11:17:44 +0100

    Added phaser code

---
+++ phaser-implementation/server/authoritative_server/js/game.js
@@ -0,0 +1,91 @@
+const config = {
+    type: Phaser.HEADLESS,
+    parent: 'phaser-example',
+    autoFocus: false,
+    width: 800,
+    height: 600,
+    physics: {
+      default: 'arcade',
+      arcade: {
+        debug: false,
+        gravity: { y: 0 }
+      }
+    },
+    scene: {
+      preload: preload,
+      create: create,
+      update: update
+    }
+};
+
+const mapInfo = {
+    nodes: [0,1,2,3],
+    edges: [(0,1),(1,2),(2,3)],
+    positions: {
+        0: (0,0),
+        1: (0,1),
+        2: (1,0),
+        3: (1,1)
+    }
+};
+
+const players = {};
+
+function preload() {
+    // Load assets
+    this.load.image('ship', 'assets/spaceShips_001.png');
+}
+function create() {
+    const self = this;
+    this.players = this.physics.add.group();
+    io.on('connection', function (socket) {
+        console.log('a user connected');
+        players[socket.id] = {
+            rotation: 0,
+            x: Math.floor(Math.random() * 700) + 50,
+            y: Math.floor(Math.random() * 500) + 50,
+            playerId: socket.id,
+            team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
+        };
+        // Add player to the server
+        addPlayer(self, players[socket.id]);
+        // Send player object to the new player
+        socket.emit('currentPlayers', players);
+        // update all other players of the new player
+        socket.broadcast.emit('newPlayer', players[socket.id]);
+        // Send the map 
+        socket.emit('newMap', mapInfo);
+
+        // Disconnection event
+        socket.on('disconnect', function () {
+            console.log('user disconnected');
+            removePlayer(self, socket.id);
+            delete players[socket.id];
+            // emit a message to trigger removal
+            socket.broadcast.emit('deletePlayer', socket.id);
+        });
+        // Create a player object
+
+
+    });
+
+}
+function update() {}
+function addPlayer(self, playerInfo){
+    const player = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+    player.setDrag(100);
+    player.setAngularDrag(100);
+    player.setMaxVelocity(200);
+    player.playerId = playerInfo.playerId;
+    self.players.add(player);
+}
+
+function removePlayer(self, playerId) {
+    self.players.getChildren().forEach((player) => {
+        if (playerId === player.playerId) {
+            player.destroy();
+        }
+    });
+}
+const game = new Phaser.Game(config);
+window.gameLoaded();
---
+++ phaser-implementation/server/public/js/game.js
@@ -0,0 +1,64 @@
+var config = {
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+var game = new Phaser.Game(config);
+function preload() {
+  this.load.image('ship', 'assets/spaceShips_001.png');
+  this.load.image('otherPlayer', 'assets/enemyBlack5.png');
+}
+
+function create() {
+  var self = this;
+  this.socket = io();
+  this.players = this.add.group();
+  
+  this.socket.on('currentPlayers', function(players) {
+    Object.keys(players).forEach(function (id) {
+      if (players[id].playerId === self.socket.id) {
+        displayPlayers(self, players[id], 'ship');
+      } else {
+        displayPlayers(self, players[id], 'otherPlayer');
+      }
+    });
+  });
+
+  this.socket.on('newPlayer', function (playerInfo) {
+    displayPlayers(self, playerInfo, 'otherPlayer');
+  });
+
+  this.socket.on('deletePlayer', function (playerId) {
+    self.players.getChildren().forEach(function (player) {
+      if (playerId === player.playerId) {
+        player.destroy();
+      }
+    });
+  });
+
+  this.socket.on('newMap', function (mapInfo) {
+    displayMap(self, mapInfo);
+  })
+}
+
+function update() {}
+
+function displayPlayers(self, playerInfo, sprite) {
+  const player = self.add.sprite(playerInfo.x, playerInfo.y, sprite).setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+  if (playerInfo.team === 'blue') player.setTint(0x0000ff);
+  else player.setTint(0xff0000);
+  player.playerId = playerInfo.playerId;
+  self.players.add(player);
+}
+
+function displayMap(self, mapInfo) {
+  mapInfo.nodes.forEach(element => {
+    var circle = new Phaser.Geom.Circle(mapInfo.positions.element[0], mapInfo.positions.element[1], 0.5);
+  });
+}
